53718bde9c0cfb3d35ea955d0f011c50ea37404e
nishantgoyal880 <nishantgoyal880@gmail.com>
2018-04-18 12:44:24 +0530

    Certified structure of angular UI og kkd-ui

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BodyComponent } from './shared/body/body.component';
+import { SearchResultComponent } from './shared/search-result/search-result.component';
+import { SearchResultListComponent } from './shared/search-result/search-result-list/search-result-list.component';
+import { HelpComponent } from './shared/help/help.component';
+import { SupportComponent } from './shared/support/support.component';
+import { AboutUsComponent } from './shared/about-us/about-us.component';
+import { PoliciesComponent } from './shared/policies/policies.component';
+import { ConnectUsComponent } from './shared/connect-us/connect-us.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    SearchResultComponent,
+    SearchResultListComponent,
+    HelpComponent,
+    SupportComponent,
+    AboutUsComponent,
+    PoliciesComponent,
+    ConnectUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
