2d69ea0f7fb4c93287947f714904e340f98f48a3
anu-bansal <anubansal0707@gmail.com>
2018-04-24 10:33:57 +0530

    adding farmer-common-header and customer-common header

--- src/app/farmer/farmer.module.ts
+++ src/app/farmer/farmer.module.ts
@@ -10,11 +10,8 @@ import { FarmerCurrentOrderComponent } from './component/farmer-dashboard/farmer
 import { FarmerPreviousOrderComponent } from './component/farmer-dashboard/farmer-previous-order/farmer-previous-order.component';
 import { FarmerViewProductComponent } from './component/farmer-dashboard/farmer-view-product/farmer-view-product.component';
 import { FarmerComponent } from './farmer.component';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
 
 
-=======
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AadhaarComponent } from './component/farmer-authentication-authorization/aadhaar/aadhaar.component';
 import { FarmerAuthenticationAuthorizationComponent } from './component/farmer-authentication-authorization/farmer-authentication-authorization.component';
@@ -22,19 +19,15 @@ import { ForgetPasswordComponent } from './component/forget-password/forget-pass
 import { FarmerDashboardComponent } from './component/farmer-dashboard/farmer-dashboard.component';
 import { FarmerHeaderComponent } from './component/farmer-header/farmer-header.component';
 import { FarmerCommonHeaderComponent } from './component/farmer-common-header/farmer-common-header.component';
->>>>>>> aca43bac10d98e8f2ede41b06ed7e5b00b146ff3
+
 
 
 @NgModule({
   imports: [
     CommonModule,
     FarmerRoutingModule,
-<<<<<<< HEAD
-    FormsModule
-=======
     FormsModule,
     ReactiveFormsModule
->>>>>>> aca43bac10d98e8f2ede41b06ed7e5b00b146ff3
   ],
 
   declarations: [FarmerLoginComponent, FarmerRegisterComponent,FarmerDashboardComponent, FarmerHeaderComponent, FarmerMyAccountComponent, FarmerAddProductComponent, FarmerBankDetailsComponent, FarmerCurrentOrderComponent, FarmerPreviousOrderComponent, FarmerViewProductComponent, FarmerComponent, AadhaarComponent, FarmerAuthenticationAuthorizationComponent, ForgetPasswordComponent,FarmerCommonHeaderComponent],
