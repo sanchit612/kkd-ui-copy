8cc3c4efeaa0ba68d0c4802635d3670dc61d6138
anu-bansal <anubansal0707@gmail.com>
2018-04-26 10:04:24 +0530

    after completing farmer-delete-profile component

--- src/app/farmer/farmer-routing.module.ts
+++ src/app/farmer/farmer-routing.module.ts
@@ -11,6 +11,10 @@ import { FarmerCurrentOrderComponent} from './component/farmer-dashboard/farmer-
 import { FarmerPreviousOrderComponent } from './component/farmer-dashboard/farmer-previous-order/farmer-previous-order.component';
 import { FarmerViewProductComponent} from './component/farmer-dashboard/farmer-view-product/farmer-view-product.component';
 import { FarmerMyAccountComponent } from './component/farmer-my-account/farmer-my-account.component';
+import { FarmerAddAddressComponent } from './component/farmer-my-account/farmer-add-address/farmer-add-address.component';
+import { FarmerAlternateMobileComponent } from './component/farmer-my-account/farmer-alternate-mobile/farmer-alternate-mobile.component';
+import { FarmerChangePasswordComponent } from './component/farmer-my-account/farmer-change-password/farmer-change-password.component';
+import { FarmerDeleteProfileComponent } from './component/farmer-my-account/farmer-delete-profile/farmer-delete-profile.component';
 
 import { AadhaarComponent } from './component/farmer-authentication-authorization/aadhaar/aadhaar.component';
 import { FarmerAuthenticationAuthorizationComponent } from './component/farmer-authentication-authorization/farmer-authentication-authorization.component';
@@ -28,6 +32,10 @@ const routes: Routes = [
 { path:'farmer/myAccount',component:FarmerMyAccountComponent },
 { path:'farmer/authenticationAndAuthorization',component:FarmerAuthenticationAuthorizationComponent },
 { path:'farmer/forgetPassword',component:ForgetPasswordComponent },
+{ path:'farmer/myAccount/addAddress',component:FarmerAddAddressComponent},
+{ path:'farmer/myAccount/alternateMobile',component:FarmerAlternateMobileComponent},
+{ path:'farmer/myAccount/changePassword',component:FarmerChangePasswordComponent},
+{ path:'farmer/myAccount/deleteProfile',component:FarmerDeleteProfileComponent },
 ];
 
 @NgModule({
