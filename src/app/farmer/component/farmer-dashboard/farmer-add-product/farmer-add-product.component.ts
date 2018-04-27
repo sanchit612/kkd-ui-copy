f83333a49847b88497f76faedfd214ac9da3fee1
Sonalkri09 <ninthjune415@gmail.com>
2018-04-27 12:09:42 +0530

    image upload in farmer-add-product component done

--- src/app/farmer/component/farmer-dashboard/farmer-add-product/farmer-add-product.component.ts
+++ src/app/farmer/component/farmer-dashboard/farmer-add-product/farmer-add-product.component.ts
@@ -29,18 +29,6 @@ export class FarmerAddProductComponent implements OnInit {
 
   ngOnInit() {
   }
-
- /* product : Product ={
-    kkdFarmId: "kkdFarmId",
-    imageUrl: "imageUrl",
-    productName: "productName",
-    description: "red",
-    price: 10,
-    bulkOrderPrice: 8,
-    quantity: 100,
-    available: false
-  }*/
-
  
 
   constructor(private productService: ProductService,private fb: FormBuilder,public router: Router) { 
@@ -61,28 +49,25 @@ export class FarmerAddProductComponent implements OnInit {
 
   onFileSelected(event: any){
     console.log(event);
-    /*if (event.target.files && event.target.files[0]) {
+    if (event.target.files && event.target.files[0]) {
       var reader = new FileReader();
   
       reader.onload = (event:any) => {
-        this.url = event.target.result;
-      }
+        this.imageUrl = event.target.result      }
   
       reader.readAsDataURL(event.target.files[0]);
-    }*/
-		//this.imageUrl=String(event.target.files[0]);
-		this.imageUrl="https://upload.wikimedia.org/wikipedia/commons/f/f0/Onions_%282272516704%29.jpg";
+    }
   }
 
   check(post){
-    alert(this.productName);
-    alert(post.available);
+    //alert(this.productName);
+    //alert(post.available);
      //alert(this.rForm.get('available'));
-     alert(post.description);
-     alert(post.price);
+    // alert(post.description);
+     //alert(post.price);
     // alert(this.bulkPrice);
     // alert(this.quantity);
-    // alert(this.imageUrl);
+     //alert(this.imageUrl);
 
     this.productSubmission = {
       
